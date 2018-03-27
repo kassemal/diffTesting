1339c3c8acdf3e4fb81fe23b29248e0407e1f4ac
Kassem <kassem>
2018-03-27 09:32:25 +0200

    Adult dataset has been added

---
+++ utils/import_adult_data.py
@@ -0,0 +1,108 @@
+"""
+This file contains functions that read "Adult" dataset and the related generalization trees. 
+
+'adult.all' file contains the training and testing datasets from http://archive.ics.uci.edu/ml/index.html
+#1. age: continuous, range = [17-90]
+#2. workclass: 'State-gov', 'Self-emp-not-inc', 'Private', 'Federal-gov', 'Local-gov', 'Self-emp-inc', 'Without-pay' (7) 
+                  (NB: + 'Never-worked' which appears in 7 lines, but all of them contain ? and thus removed ==> nb_catagories = 7) 
+#3. final_weight: continuous, range = [13769-1484705]
+#4. education: Bachelors, Some-college, 11th, HS-grad, Prof-school, Assoc-acdm, 
+                  Assoc-voc, 9th, 7th-8th, 12th, Masters, 1st-4th, 10th, Doctorate, 5th-6th, Preschool. (16)
+#5. education_num: continuous, range = [1-16]
+#6. marital_status: 'Never-married', 'Married-civ-spouse', 'Divorced', 'Married-spouse-absent', 'Separated', 'Married-AF-spouse', 'Widowed'. (7)
+#7. occupation: 'Adm-clerical', 'Exec-managerial', 'Handlers-cleaners', 'Prof-specialty', 'Other-service', 'Sales', 'Transport-moving', 
+                   'Farming-fishing', 'Machine-op-inspct', 'Tech-support', 'Craft-repair', 'Protective-serv', 'Armed-Forces', 
+                   'Priv-house-serv'. (14)
+#8. relationship: Wife, Own-child, Husband, Not-in-family, Other-relative, Unmarried. (6)
+#9. race: 'White', 'Black', 'Asian-Pac-Islander', 'Amer-Indian-Eskimo', 'Other'. (5)
+#10. sex: 'Male', 'Female'. (2)
+#11. capital_gain: continuous, range = [0-99999]
+#12. capital_loss: continuous, range = [0-4356]
+#13. hours_per_week: continuous, range = [1-99]
+#15. native_country: 'United-States', 'Cuba', 'Jamaica', 'India', 'Mexico', 'Puerto-Rico', 'Honduras', 'England', 'Canada', 'Germany', 
+                         'Iran', 'Philippines', 'Poland', 'Columbia', 'Cambodia', 'Thailand', 'Ecuador', 'Laos', 'Taiwan', 'Haiti', 'Portugal', 
+                         'Dominican-Republic', 'El-Salvador', 'France', 'Guatemala', 'Italy', 'China', 'South', 'Japan', 'Yugoslavia', 'Peru', 
+                         'Outlying-US(Guam-USVI-etc)', 'Scotland', 'Trinadad&Tobago', 'Greece', 'Nicaragua', 'Vietnam', 'Hong', 'Ireland', 
+                         'Hungary', 'Holand-Netherlands'. (41)
+#15. income_level: '<=50K', '>50K' (2)
+"""
+
+#!/usr/bin/env python
+# coding=utf-8
+#
+from utils.utility import *
+from pulp import *
+#
+#Attribute names as ordered in 'data/adult.data' file 
+ATT_NAMES_adult = ['age', 'workclass', 'final_weight', 'education',
+             'education_num', 'marital_status', 'occupation', 'relationship',
+             'race', 'sex', 'capital_gain', 'capital_loss', 'hours_per_week',
+             'native_country', 'income_level']     
+#'False' means that the attribute values are continuous or ordinal, and 'True' means that the attribute is categorial 
+CATEGORY_adult = [False, True, False, True, False, True, True,  True,  True,  True,  False,  False,  False,  True,  True]
+#
+#
+def read_adult_data():
+    """
+    This method reads "Adult" dataset from 'data/adult.data'
+    """
+    nb_attributes = len(ATT_NAMES_adult)
+    data, numeric_dict = [], []
+    for i in range(nb_attributes):
+        if CATEGORY_adult[i] is False:
+       		numeric_dict.append(dict()) #dictionary for continuous attributes
+    #read data
+    data_file = open('data/adult.all', 'rU')
+    for line in data_file:
+        line = line.strip()
+        #remove empty and incomplete lines, Only 45222 records will remain 
+        if len(line) == 0 or '?' in line:
+            continue
+        #remove spaces
+        line = line.replace(' ', '')
+        #split
+        temp = line.split(',')
+        #append to table $data
+        data.append(temp)
+        #keep a dictionary of numerical attributes
+        index = 0
+        for i in range(nb_attributes):
+            if CATEGORY_adult[i] is False:
+                try:
+                    numeric_dict[index][temp[i]] += 1
+                except:
+                    numeric_dict[index][temp[i]] = 1
+                index += 1
+    #pickle numeric attributes and get NumRange
+    index = 0
+    for i in range(nb_attributes):
+        if CATEGORY_adult[i] is False:
+            static_file = open('data/adult_' + ATT_NAMES_adult[i] + '_static.pickle', 'wb')
+            sort_value = list(numeric_dict[index].keys())
+            sort_value.sort(cmp=cmp_str)
+            pickle.dump((numeric_dict[index], sort_value), static_file)
+            static_file.close()
+            index += 1
+    return data
+
+def read_adult_tree(att_names):
+    """
+    This method reads, for every attribute in $att_names, the related tree from data/tree_*.txt, 
+    and stores it in $att_trees.
+    Moreover, it stores the the paths to leaves in the case of categorical attribute. 
+    """
+    prefix = 'data/adult_'
+    att_trees = []
+    paths_to_leaves = []
+    for i in range(len(att_names)):
+        if CATEGORY_adult[ATT_NAMES_adult.index(att_names[i])]:
+            att_tree,  path_to_leaf = read_tree_file(att_names[i], prefix)
+            att_trees.append(att_tree)
+            paths_to_leaves.append(path_to_leaf)
+        else:
+            att_tree = read_pickle_file(att_names[i], prefix)
+            att_trees.append(att_tree)
+    return att_trees,  paths_to_leaves
+    
+    
+
---
+++ utils/utility.py
@@ -0,0 +1,82 @@
+"""
+public functions
+
+This code taken from 
+https://github.com/qiyuangong/Basic_Mondrian
+under the MIT License (MIT)
+"""
+
+# !/usr/bin/env python
+# coding=utf-8
+from models.gentree import GenTree
+from models.numrange import NumRange
+import pickle
+
+__DEBUG = False
+
+def cmp_str(element1, element2):
+    """
+    compare number in str format correctley
+    """
+    try:
+        return cmp(int(element1), int(element2))
+    except ValueError:
+        return cmp(element1, element2)
+
+def list_to_str(value_list, cmpfun=cmp, sep=';'):
+    """covert sorted str list (sorted by cmpfun) to str
+    value (splited by sep). This fuction is value safe, which means
+    value_list will not be changed.
+    return str list.
+    """
+    temp = value_list[:]
+    temp.sort(cmp=cmpfun)
+    return sep.join(temp)
+
+def read_pickle_file(att_name, prefix):
+    """
+    read pickle file for numeric attributes
+    return numrange object
+    """
+    try:
+        static_file = open(prefix + att_name + '_static.pickle', 'rb')
+        (numeric_dict, sort_value) = pickle.load(static_file)
+        static_file.close()
+        result = NumRange(sort_value, numeric_dict)
+        return result
+    except:
+        print "Pickle file not exists!!"
+
+def read_tree_file(att_name, prefix):
+    """read tree data from prefix + att_name + postfix
+    """
+    path_to_leaf = dict()
+    att_tree = {}
+    postfix = ".txt"
+    treefile = open(prefix + att_name + postfix, 'rU')
+    att_tree['*'] = GenTree('*')
+    if __DEBUG:
+        print "Reading Tree" + att_name
+    for line in treefile:
+        # delete \n
+        if len(line) <= 1:
+            break
+        line = line.strip()
+        temp = line.split(';')
+        path_to_leaf[temp[0]] = temp
+        # copy temp
+        temp.reverse()
+        for i, t in enumerate(temp):
+            isleaf = False
+            if i == len(temp) - 1:
+                isleaf = True
+            # try and except is more efficient than 'in'
+            try:
+                att_tree[t]
+            except:
+                att_tree[t] = GenTree(t, att_tree[temp[i - 1]], isleaf)
+    if __DEBUG:
+        print 'Nodes No. = %d' % att_tree['*'].support
+    treefile.close()
+    #print att_tree
+    return att_tree,  path_to_leaf
