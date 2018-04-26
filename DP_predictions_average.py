9f9ed9c5f5a3e711f440f99ea47a8f636a647815
Kassem <alikassem20@gmail.com>
2018-04-26 11:12:39 +0200

    polish the code

--- DP_predictions_average.py
+++ DP_predictions_average.py
@@ -1,188 +1,240 @@
 """
-This program aims to study the effect of the existence of a certain record inside 
-a certain dataset on the predictions that are made by a classifier that is trained using this dataset. 
-It considered a part from UCI Adult dataset with only two attributes: 'occupation' that is used as a 
-quasi-identifier (this works as an input for the classifier) and 'income_level' that is used as a sensitive 
-value (the classifier has to predict the sensitive value for a given quasi-identifier). 
-The considered classifier takes as input a quasi-identifier from a set Q and 
-outputs a prediction distribution Pr[S|Q=q] where S is the set of all possible sensitive values.  
-Note that, that the probabilities are computed after adding Laplacian noise to the counts of (q,s) 
-inside the dataset.
+================================================================================================
+Apply Differential Testing on the results of differentially private queries on UCI Adult dataset
+================================================================================================
 
-More precisely, it does the following: 
-1- Read some already selected Adult records from a file or select new $size 
-   records from 'data/adult.all' based on the value of the variable $Select. 
-2- For every distinct record (q,s), it makes $NB_Iterations prediction distributions while considering the full dataset 
-   (i.e. while this record exists). It then takes their average in order to obtain a distribution P. 
-   After removing this record from the dataset, it repeats the same process in 
-   order to obtain a probability distribution P'. 
-3- For every record, it computes a distance between the corresponding probability distributions P and P'.
-   Steps 2 and 3 can be repeated for more than one distance computation method and more than one noise parameter epsilon. 
+Differential privacy is different from generalization techniques, like k-anonymity and l-diversity, 
+in that it does not explicitly publish microdata. Instead, a user makes a query on the data, the 
+server then computes the exact answer and adds some noise before replying. 
+We consider count queries, that is in the form of "What is the number of individuals in the data that 
+satisfy certain conditions". Moreover, we assume histogram queries, that is only a single round of 
+querying without any kind of adaptive querying in response to previous results. 
+
+The idea is to use the queries results ("the noised counts") to build a classifier that takes as input 
+a tuple of quasi-identifiers and outputs a prediction distribution on the sensitive attribute values. 
+
+The considered classifier is a Naive Bayes one which outputs, for a given quasi-identifier tuple q from 
+a set Q, a prediction distribution Pr[S|Q=q] where S is the set of all possible sensitive values.  
+As a noise, random values from a Laplacian distribution are considered. 
+
+In more details, do the following: 
+1- Read the full UCI Adult dataset from 'data/adult.all', pick up only the attributes in $ATT_QI + 
+   $ATT_SA, and randomly select $SIZE records. Moreover, write the selected dataset into $filename.
+   Then, quantize attributes in $ATT_CONTINUOUS. 
+   Note that, to read an already selected dataset one has to set $IS_SELECT_NEW to False.  
+
+2- For every distinct record (q,s), make $ITERATIONS_NB prediction distributions while considering 
+   the entire dataset (i.e. while this record exists). Then take their average in order to obtain 
+   a distribution P. After removing this record from the dataset, repeat the same process in order 
+   to obtain a probability distribution P_w. 
+
+3- For every record, compute a distance between the corresponding distributions P and P_w.
+   Repeat steps 2 and 3 for every distance computation method in $DISTANCE_TAGS 
+   and for every noise parameter epsilon in $EPSILON_VALUES. 
+
 4- Plot the cumulative relative frequency of the computed distances for all epsilon values in one graph. 
    A graph is plotted for each distance method. 
 """ 
-#
+
 # !/usr/bin/env python
 # coding=utf-8
-#
-from utils.methods import import_data, compute_counts, get_att_values
-from utils.methods import create_file
-import numpy as np
-import copy, math
-from pyemd import emd
-from scipy.stats import entropy
-from math import sqrt,  log 
-from random import randint
+
+import pdb
 import matplotlib.pyplot as plt
+import numpy as np
+import pyemd
+import random
+import time 
 #
-def compute_conditional_probabilities(q, counts, epsilon): 
+from utils import methods
+
+
+def conditional_probabilities_compute(quasi_id, counts, epsilon): 
 	"""
-	This method computes the probabilities Pr[s|$q] for all pairs ($q,s) whose 
+	Compute the probabilities Pr[s|$quasi_id] for all pairs ($quasi_id,s) whose 
 	frequencies are inside dictionary $counts.  
+
+	e.g. 
+	conditional_probabilities_compute(
+	                                  quasi_id='Female,White', 
+	                                  counts={'Female,White;<=50K': 43, 'Female,White;>50K': 26}, 
+	                                  epsilon=0.1)
+	rType: dictionary of probabilities 
 	"""
 	mean = 0.0
 	decay = 1.0/epsilon
 	total_q = 0.0
 	#compute the total number of records (summation of all frequencies inside $counts)
-	#this is equal to the size of the dataset ($Size)
+	#this is equal to the size of the dataset ($SIZE)
 	nb_records_q = 0
 	for key, value in counts.iteritems():
-		if q == key.split(';')[0]: 
+		if quasi_id == key.split(';')[0]: 
 			nb_records_q += value
-	#compute probabilities
+	#compute probabilities Pr[s|$quasi_id] for every possible sensitive value s
 	probabilities = dict()
 	for key, value in counts.iteritems():
-		if q == key.split(';')[0]: 
+		if quasi_id == key.split(';')[0]:
 			noise = np.random.laplace(loc=mean, scale=decay)
-			term = 1.0 + min(max(0, value + noise), nb_records_q) #The addition of 1 is just for correction. 
-			probabilities[key] = term                                #Also to avoid having total = 0.0
+			term = 1.0 + min(max(0, value + noise), nb_records_q) #The addition of 1 is for correction. 
+			probabilities[key] = term 
 			total_q += term
 	for key, value in probabilities.iteritems():
 		probabilities[key] = value/total_q
 	return probabilities
-#
-def compute_proba_distribution(q, counts, epsilon, N, sa_values): 
+
+
+def proba_distribution_compute(quasi_id, counts, epsilon, n, sa_values): 
 	"""
-	This method obtains a prediction distribution for quasi-identifier $q after making 
-	$N predictions based on the records distribution $counts inside the related dataset. 
+	Obtain a prediction distribution for quasi-identifier $quasi_id after making 
+	$n predictions based on the records distribution $counts inside the related dataset. 
 	"""
-	pred_proba = [0.0 for s in sa_values]
-	#obtain $N predictions for $q
-	for i in range(N):
-		#compute all probabilities Pr[s|$q] for all SA s
-		probabilities = dict()
-		probabilities = compute_conditional_probabilities(q, counts, epsilon)
+	distribution = [0.0 for s in sa_values]
+	#obtain $n prediction distributions for $quasi_id
+	for i in range(n):
+		#compute all probabilities Pr[s|$quasi_id] for every sensitive value s
+		probabilities = conditional_probabilities_compute(quasi_id, counts, epsilon)
 		#add the probabilities to the total summation
 		for i, s in enumerate(sa_values):
-			pred_proba[i] += probabilities[q+';'+s]
-	return [x/float(N) for x in pred_proba] #normalize the summations
-#
-def distance_D(pd1, pd2, nb_classes, N, D='EMD'):
+			distribution[i] += probabilities[quasi_id+';'+s]
+	return [x/float(n) for x in distribution] #normalize the summations
+
+
+def distance_compute(pd1, pd2, classes_nb, d_tag='EMD', infinity=1000):
 	"""
-	Computes distance between two probability distributions. 
-	$N is the total number of predictions that have been used to obtain pd1 and pd2. 
-	$N is used to denote the infinity when a division by 0 is encountered. 
+	Compute distance between two probability distributions $pd1 and $pd2. 
+
+	e.g. distance_compute([0.45, 0.55], [0.2, 0.8], classes_nb=2)
+	rType: float, a distance
 	"""
 	distance = 0.0
-	if D == 'EMD':
+	#Switch cases according to the $d_tag
+	if d_tag == 'EMD':
 	    #An equal ground distance is taken between any two different attribute values. 
-	    matrix = []
-	    for i in range(nb_classes):
-	        raw = []
-	        for j in range(nb_classes):
+	    ground_matrix = []
+	    for i in range(classes_nb):
+	        row = []
+	        for j in range(classes_nb):
 	            if j == i:
-	                raw.append(0.0)
+	                row.append(0.0)
 	            else:
-	                raw.append(1.0)
-	        matrix.append(raw)
-	    matrix_d = np.array(matrix) # ground distance matrix for EMD
-	    distance = emd(np.array(pd1), np.array(pd2), matrix_d)
-	elif D == 'ratio':
-		for i in range(nb_classes):
-			mr = 0.0   
+	                row.append(1.0)
+	        ground_matrix.append(row)
+	    ground_matrix = np.array(ground_matrix) # ground distance matrix for EMD
+	    distance = pyemd.emd(np.array(pd1), np.array(pd2), ground_matrix)
+	elif d_tag == 'm_ratio':#obtain maximal ratio
+		for i in range(classes_nb):
+			max_r = 0.0   
 			if (pd1[i] == 0 and pd2[i] != 0) or (pd1[i] != 0 and pd2[i] == 0):
-				mr = 5#N
+				max_r = infinity 
 			elif pd1[i] == 0 and pd2[i] == 0:
-				mr = 1
+				max_r = 1.0
 			elif pd1[i] != 0 and pd2[i] != 0:
-				mr = max(pd1[i]/pd2[i], pd2[i]/pd1[i])
+				max_r = max(pd1[i], pd2[i])/min(pd1[i], pd2[i])
 			#
-			if mr > distance:
-				distance = mr    
+			if max_r > distance:
+				distance = max_r    
 	return distance 
-#
-#ATT_NAMES_adult = ['age', 'workclass', 'final_weight', 'education',
+
+
+#ATT_NAMES= ['age', 'workclass', 'final_weight', 'education', 
 #             'education_num', 'marital_status', 'occupation', 'relationship',
 #             'race', 'sex', 'capital_gain', 'capital_loss', 'hours_per_week',
 #             'native_country', 'income_level']   
-QI_ATT = ['occupation'] #quasi-identifier attributes
-SA_ATT = ['income_level'] #sensitive attributes
-Name = 'adult' #name of the dataset
-Size = 1000 #size of the dataset to consider
-Select = False #True is to select new data
-NB_Iterations = 50000 #number of predictions to make for each record
-Epsilons = [0.01, 0.05, 0.1, 0.2, 0.3] #[0.01, 0.1, 1.0, 2.0] #noise parameter values to consider
-color_list  = {0.01:'c', 0.05:'b', 0.1: 'r', 0.2:'g', 0.3:'k'} 
-marker_list = {0.01:'x', 0.05:'o', 0.1: 's', 0.2:'>', 0.3:'*'} 
-D = ['EMD', 'ratio'] #distances to consider
-#
-if __name__ == '__main__':
+ATT_QI = ['age', 'education', 'occupation', 'sex'] #quasi-identifier attributes
+ATT_SA = 'income_level' #sensitive attributes
+ATT_CONTINUOUS = ['age'] 
+QUANTILES = [
+            [[0,25],[26,50],[51,75],[75,100]]
+            ]
+NAME = 'adult' #name of the dataset
+SIZE = 1000 #size of the dataset to consider
+IS_SELECT_NEW = True #True is to select new data
+ITERATIONS_NB = 1000 #number of predictions to make for each record
+EPSILON_VALUES = [0.01, 0.05, 0.1, 0.3, 1.0] #noise parameter to consider
+COLOR_LIST  = {0.01:'c', 0.05:'b', 0.1: 'r', 0.2:'g', 0.3:'k', 1.0:'y'} 
+DISTANCE_TAGS = ['EMD', 'm_ratio'] #distances to consider: EMD for Earth Mover Distance, and m_ratio for Maximal ratio
 
+
+if __name__ == '__main__':
+	#pdb.set_trace()
+	start_time = time.time()
 	#1- Import data
 	print 'Read data ...'
-	selected_DATA = []
-	filename = 'results/selected_DATA/%s/testing_data'%Name #selected_%s_S%s'%(Name,Name,str(Size))
-	selected_DATA = import_data(Select, name=Name, size=Size, qi_list=QI_ATT, sa_list=SA_ATT, filename=filename)
+	dataset = [] # a dataset is a table
+	filename = 'results/selected_DATA/%s/selected_%s_S%s'%(NAME, NAME, str(SIZE))
+	#read and filter the dataset
+	dataset = methods.data_import(IS_SELECT_NEW, name=NAME, size=SIZE, qi_list=ATT_QI, sa=ATT_SA, filename=filename)
+	#quantize attributes in $ATT_CONTINUOUS
+	if all(x in ATT_CONTINUOUS for x in ATT_QI):
+		methods.data_quantize(dataset, [ATT_QI.index(x) for x in ATT_CONTINUOUS], QUANTILES) #directly modifies $dataset
+
+	#2- Build classifier and make predictions
+	#obtain sensitive attribute values
+	sa_values = methods.att_values_get(dataset, -1)
+	#obtain noised counts
+	counts = methods.counts_compute(dataset)
+	print 'N=', ITERATIONS_NB
+	counter = 0
+	for key, value in counts.iteritems():
+		if value == 0:
+			counter += 1
+	print 'counts', counter
+	print 'max', max([x for x in counts.itervalues()])
+	print 'min', min([x for x in counts.itervalues()])
+	#initiate a distances dictionary 
+	distances_dict = dict()
+	for d_tag in DISTANCE_TAGS:
+		distances_dict[d_tag] = []
 	#
-	#2- Add noise and compute prediction probability distributions
-	QI_num = len(QI_ATT)
-	SA_num = len(SA_ATT)
-	COUNTS = compute_counts(selected_DATA, QI_num, SA_num)
-	att_values = get_att_values(selected_DATA)
-	Distances_all_D = dict()
-	for xD in D:
-		Distances_all_D[xD] = []
-	for epsilon in Epsilons:
+	for epsilon in EPSILON_VALUES:
 		print 'Predictions for epsilon %s ...'%str(epsilon)
-		predicted_proba, predicted_proba_w = [], []
-		for key in COUNTS:
-			q = key.split(';')[0]
-			#obtain $NB_Iterations with the record
-			proba = compute_proba_distribution(q, COUNTS, epsilon, NB_Iterations, att_values[-1])
-			predicted_proba.append(proba)
-			#obtain $NB_Iterations without the record
-			COUNTS[key] -= 1
-			proba_w = compute_proba_distribution(q, COUNTS, epsilon, NB_Iterations, att_values[-1])
-			predicted_proba_w.append(proba_w) 
-			COUNTS[key] += 1
+		predicted_distributions, predicted_distributions_w = [], []
+		#for every distinct quasi-identifiers tuple $quasi_id do: 
+		for key, value in counts.iteritems():
+			if value != 0: #if the record exist in the dataset 
+				quasi_id = key.split(';')[0]
+				#while considering the entire dataset, obtain $ITERATIONS_NB prediction distributions and get their average
+				distribution = proba_distribution_compute(quasi_id, counts, epsilon, ITERATIONS_NB, sa_values)
+				predicted_distributions.append(distribution)
+				#remove one record that has $quasi_id, then obtain $ITERATIONS_NB prediction distributions and get their average
+				counts[key] -= 1
+				distribution_w = proba_distribution_compute(quasi_id, counts, epsilon, ITERATIONS_NB, sa_values)
+				predicted_distributions_w.append(distribution_w) 
+				counts[key] += 1
+		#write the obtained predictions into $predictions_file
+		predictions_file = 'results/predictions/DP/%s/predictions_%s_S%s_N%s_eps%s'%(NAME, NAME, str(SIZE), str(ITERATIONS_NB), str(epsilon))
+		methods.table_write([predicted_distributions[i]+predicted_distributions_w[i] for i in range(len(predicted_distributions))], predictions_file)
 		#
 		#3- Compute distances
-		for xD in D:
-			Distance_pp = []
-			for j in range(len(predicted_proba)):
-			    d = distance_D(predicted_proba[j], predicted_proba_w[j], len(att_values[-1]), NB_Iterations, xD)
-			    Distance_pp.append(d)
-			Distances_all_D[xD].append(Distance_pp)
+		for d_tag in DISTANCE_TAGS:
+			distances = []
+			#for every distinct quasi-identifiers tuple, compute the distance between the corresponding prediction distributions
+			for j in range(len(predicted_distributions)):
+			    d = distance_compute(predicted_distributions[j], predicted_distributions_w[j], len(sa_values), d_tag)
+			    distances.append(d)
+			distances_dict[d_tag].append(distances)
+	comp_time = time.time() - start_time 
+	print 'Computation time in seconds:',  comp_time 
 	#4- Plot the relative CDF of the distances  
-	for xD in D:
-		curves, leg = [], []
+	for d_tag in DISTANCE_TAGS:
+		curves, legend = [], []
 		fig = plt.figure()  
-		curves = [0 for j in range(len(Epsilons))]
-		leg = ['eps = ' + str(x) for x in Epsilons]
+		curves = [0 for x in range(len(EPSILON_VALUES))]
+		legend = ['eps = ' + str(x) for x in EPSILON_VALUES]
 		index = 0
-		for i in range(len(Distances_all_D[xD])):
-		    size = len(Distances_all_D[xD][i])
+		for i in range(len(distances_dict[d_tag])):
+		    size = len(distances_dict[d_tag][i])
 		    yvals = np.arange(1,  size+1)/float(size)
-		    curves[index],  = plt.plot(np.sort([x for x in Distances_all_D[xD][i]]), yvals, color_list[Epsilons[i]], marker=marker_list[Epsilons[i]], label=Epsilons[i])
+		    curves[index],  = plt.plot(np.sort([x for x in distances_dict[d_tag][i]]), yvals, COLOR_LIST[EPSILON_VALUES[i]], label=EPSILON_VALUES[i])
 		    index += 1
-		plt.legend([x for x in curves],leg,loc=4,fontsize=12,frameon=False)
-		#plt.xlim(min([min(x) for x in Distances_all_D][xD]), max([max(x) for x in Distances_all_D][xD]))
-		plt.xlabel('%s'%xD, fontsize=14)
-		plt.ylabel('Cumulative Relative Frequency',fontsize=14)  
-		plt.title('Size=%dK, N=%dK'%(Size/1000,NB_Iterations/1000))
+		plt.legend([x for x in curves], legend, loc=4, fontsize=12, frameon=False)
+		#plt.xlim(min([min(x) for x in distances_dict][d_tag]), max([max(x) for x in distances_dict][d_tag]))
+		plt.xlabel('%s'%d_tag, fontsize=14)
+		plt.ylabel('Cumulative Relative Frequency', fontsize=14)  
+		plt.title('Size=%dK, N=%dK'%(SIZE/1000, ITERATIONS_NB/1000))
 		fig = plt.gcf()
-		filename = 'results/figures/cdf/%s/average/cdf_DP_%s_S%d_N%d_%s_average.pdf'%(Name,Name,Size,NB_Iterations,xD)
-		create_file(filename)
+		filename = 'results/figures/cdf/%s/average/cdf_DP_%s_S%d_N%d_%s_average.pdf'%(NAME, NAME, SIZE, ITERATIONS_NB, d_tag)
+		methods.file_create(filename)
 		fig.savefig(filename, bbox_inches='tight')
 		plt.show()
 		plt.close(fig)
--- utils/methods.py
+++ utils/methods.py
@@ -1,140 +1,146 @@
 """
-Methods
+This file contains methods related to read, write, manipulate and perform counts on datasets/tables. 
 """
-#
+
+
 # !/usr/bin/env python
 # coding=utf-8
-from import_adult_data import *
 from pulp import *
 import random 
+import random
 #
-def select_attributes(data, att_names):
+import adult_data
+
+
+def attributes_filter(dataset, att_names):
     """
-    This method selects (in order) from $data only the attributes whose names are in $att_names. 
-    Note that, this method assumes that $data contains all the attributes from $ATT_NAMES_adult
-    (see utils/import_adult_data.py) 
+    Pick up from $dataset only the attributes whose names are in $att_names. 
+    Note that, this method assumes that $dataset contains all the attributes from $name_data.ATT_NAMES
     """
-    global ATT_NAMES_adult #from import_adult_data.py
     #obtain attribute indices
     att_indices = []
     for name in att_names:
-        att_indices.append(ATT_NAMES_adult.index(name))
+        att_indices.append(adult_data.ATT_NAMES.index(name))
     #obtain related columns 
-    res_data = []
-    for record in data:
+    res_dataset = []
+    for record in dataset:
         temp = []
         for i in att_indices:
             temp.append(record[i])
-        res_data.append(temp)
-    return res_data
-#
-def select_records(data, size):
+        res_dataset.append(temp)
+    return res_dataset
+
+
+def records_select(dataset, size):
     """
-    This method takes a table $data and selects only $size rows from it.
+    Take a table $dataset and selects only $size rows from it.
     """
     #Randomly select $size lines from data, if size < len(data). Otherwise, take all the data.
     counter = 0
-    selected_data = []
-    length = len(data)
-    if size < length:
+    selected_dataset = []
+    if size < len(dataset):
         counter = 0
         while(counter < size):
-            i = random.choice(range(len(data)))
-            selected_data.append(data.pop(i))
+            i = random.choice(range(len(dataset)))
+            selected_dataset.append(dataset.pop(i))
             counter += 1
-    else: # if size > length, take all data
-        selected_data = data
-    return selected_data
-#
-def import_data(select_new, name='adult', size=1000, qi_list=['occupation'], sa_list=['income_level'], filename='results/selected_data'):
+    else: # if size > len(dataset), take all data
+        selected_dataset = dataset
+    return selected_dataset
+
+
+def data_import(is_select_new, name='adult', size=1000, qi_list=['occupation'], sa='income_level', filename='results/selected_data'):
     """
-    This method imports a dataset from a file. 
-    It either reads already selected records from $filename if $select_new=False, or 
-    selects $size records from dataset $name while considering only $qi_list and $sa_list if $select_new=True. 
-    Note that in the latter case (when $select_new=True) the selected data are written into $filename.
+    Import a dataset from a file: 
+    either read already prepared data from $filename if $is_select_new=False, or 
+    select $size records from dataset $name while considering only $qi_list and $sa_list if $is_select_new=True. 
     """
-    selected_data = []
-    if select_new == True:#select new data
-        data = []
+    dataset = []
+    if is_select_new == True:#select new data
         if name == 'adult':
-            data = read_adult_data() #get full Adult dataset
+            dataset = adult_data.read() #get full Adult dataset
         else:
             print 'The dataset "%s" doesn\'t exist!'%name
             return []
-        selected_att = [] 
-        selected_att = select_attributes(data, qi_list + sa_list) #select specific attributes
-        selected_data = select_records(selected_att, size)#select $size records
+        #pick up $qi_list+$sa attributes
+        dataset = attributes_filter(dataset, qi_list + [sa]) 
+        #select $size records
+        dataset = records_select(dataset, size)
         #write selected data into a file
-        write_data(selected_data, filename)
+        table_write(dataset, filename)
     else:
         #Read already selected data from $filename
         data_file = open(filename, 'r')
         for line in data_file:
             line = line.strip()
             line = line.split(' ')
-            selected_data.append(line)
-    return selected_data
-#
-def get_att_values(data):
+            dataset.append(line)
+    return dataset
+
+
+def data_quantize(dataset, att_indices, quantiles):
+    for i in range(len(dataset)):
+        for j, index in enumerate(att_indices):
+            val = None
+            for k, interval in enumerate(quantiles[j]): 
+                if interval[0] <= int(dataset[i][index]) <= interval[1]:
+                    val = k
+            if val is None:
+                print 'The value %s of the attribute with index %d doesn\'t belong to any interval!'%(dataset[i][index],index)
+            else:
+                dataset[i][index] = '%s'%val
+    return None
+
+
+def att_values_get(dataset, index=-1):
     """
-    This method takes a table $data and return all values that are taken by each attribute. 
+    Take a table $dataset and return all the possible value of the attribute with index $index. 
     """
-    if len(data) == 0:
-        return []
-    else:
-        att_values = [[] for x in range(len(data[0]))]
-        for record in data:
-            for j in range(len(data[0])):
-                if record[j] not in att_values[j]:
-                    att_values[j].append(str(record[j]))
+    att_values = []
+    if len(dataset) == 0:
         return att_values
-#
-# def compute_counts(data,  q_values, s_values): #cij
-#    """
-#    This method computes, for every pair (q, s) belongs to (q_values, s_values), the number 
-#    of records for which the quasi-identifier value is q and the sensitive value is s.
-#    This method assumes one quasi-identifier and one sensitive attribute. Those can be tuples for 
-#    several values. 
-#    """
-#    #initiate all counts to zero   
-#    counts = dict()     
-#    for q in q_values:
-#        for s in s_values:
-#            counts[str(q) + ';' + s] = 0
-#    #compute counts
-#    for record in data: 
-#        counts[str(record[0]) + ';' + record[-1]] += 1
-#    return counts
-#
-def compute_counts(data, qi_num, sa_num): #cij
+    else:
+        for row in dataset:
+            if row[index] not in att_values:
+                att_values.append(row[index])
+        return att_values
+
+
+def qi_concatenate(dataset, qi_num=-1):
+    """ 
+    Take a $dataset and concatenate first $qi_num attribute values.
+    Consider only distinct records. 
     """
-    This method computes, for every pair (q, s), the number of records for which the 
+    qi_concatenation = []
+    for row in dataset:
+        qi = ','.join(str(x) for x in row[0:qi_num])
+        if qi not in qi_concatenation:
+            qi_concatenation.append(qi)
+    return qi_concatenation
+
+
+def counts_compute(dataset, qi_num=-1, sa_index=-1): 
+    """
+    For every distinct pair (q, s) in $dataset, compute the number of records for which the 
     quasi-identifier(s) tuple (first $qi_num attributes) is equal to q and the sensitive value(s) 
     tuple (last $sa_num attributes) is equal to s.
-    Note that this method considers only the first record when we have several identical records. 
     """
-    counts = dict()     
-    indices = []
-    for i in range(len(data)): 
-        if i in indices: #skip records that are identical to the already considered ones
-            continue
-        counter = 0
-        for j in range(len(data)):
-            if data[j] == data[i]:
-                counter += 1
-                if j != i:
-                    indices.append(j)
-        if len(data[i]) != qi_num + sa_num:
-            print "The number of attributes in the record %d is not equal to the summation of QI and SA attributes."%(i+1)
-            break
-        else:
-            key = ','.join(str(x) for x in data[i][0:qi_num]) + ';' + ','.join(str(x) for x in data[i][qi_num:])
-            counts[key] = counter
+    qi_concatenation = qi_concatenate(dataset)
+    sa_values = att_values_get(dataset, sa_index)
+    counts = dict()  
+    for q in qi_concatenation:
+        for s in sa_values:
+            counts[q + ';' + s] = 0
+    #
+    for i in range(len(dataset)):
+        key = ','.join(str(x) for x in dataset[i][0:qi_num]) + ';' + ','.join(str(x) for x in dataset[i][qi_num:])
+        counts[key] += 1
     return counts
-#
-def create_file(filename):
+
+
+def file_create(filename):
     """
-    This method creates all directories in path to filename, if not exist.
+    Create all the directories in path to filename, if not exist.
     """
     if os.path.basename(filename) == filename:
         return 0
@@ -145,17 +151,18 @@ def create_file(filename):
             except OSError as exc: # Guard against race condition
                 if exc.errno != errno.EEXIST:
                     raise
-    return 0
-#
-def write_data(data,  filename):
+    return None
+
+
+def table_write(table,  filename):
     """
-    This method writes table $data into $filename. 
-    The different attributes will be separated by spaces. 
+    Write $table into $filename. 
+    A row per line and separate different attribute values by spaces.
     """
-    create_file(filename)
+    file_create(filename)
     f = open(filename, 'w')
-    for line in data:
+    for line in table:
         f.write(' '.join(str(x) for x in line) + '\n')
     f.close()
-    return 0 
+    return None
     
